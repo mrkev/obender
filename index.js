e4424a2999508f2fafb8c26cc1a7197297249f65
Kevin <mr.kev@me.com>
2014-07-07 20:15:32 -0500

    Refactor remap() and finish tests. YAYZERS

--- index.js
+++ index.js
@@ -1,31 +1,133 @@
-module.exports.remap = function(map, obj) {
-	if (obj === null ||
-		obj === undefined) {
-		return;
-	}
-	for (var _old in map) {
-		if (obj[_old] === undefined) continue;
-		
-		if (typeof map[_old] === 'string') {
-			var _new = map[_old];
-			obj[_new] = obj[_old];
-			delete obj[_old];
-		} else 
+/* global module, console */
+'use strict';
 
-		if (typeof map[_old] === 'object') {
-			var _nkey = Object.keys(map[_old])[0];
-			var _nval = map[_old][_nkey](obj[_old]);
-			obj[_nkey] = _nval;
-			delete obj[_old];
+// var _ = require('underscore');
+// { '*'  : ['string', 'number', 'function', {'*' : ['function', 'number', 'string']}] }
+module.exports.remap = function(map, obj) {
+	if (obj === null || obj === undefined ||
+		map === null || map === undefined) { 
+		return; }
+
+
+	for (var _okey in map) {
+
+		// There's no such key to map. Move on.
+		if (!obj.hasOwnProperty(_okey)) continue;
+
+
+		var _nkey = map[_okey];
+
+		// Key is mapped to nothing. Move on. To map to 'null' and 'undefined'
+		// user should use literal strings.
+		if (_nkey === undefined ||
+			_nkey === null) continue;
+
+
+		// If we're mapping to a non-primitive encapsulating object, we'll try
+		// to use it.
+		if (typeof _nkey === 'object' && 
+			_nkey.constructor === Object) {	
+
+
+			//
+			// {'Color' : {'key' : 'color', 
+			// 			   'val' :'yo'}}
+			//
+			// 
+
+			var _key = _nkey.key;
+			var _val = _nkey.val;
+
+			// We'll do nothing with the key because that will be dealt with
+			// outside this 'if'. Lets check out the value though.
+			
+			// Hmm value isn't anything? Must be in the compact syntax then.
+			if (_val === undefined) {
+				_key = Object.keys(_nkey)[0];
+				_val = _nkey[_key];
+			}
+			
+
+			// If new value is a function, evaluate it.
+			if (typeof _val === 'function') {
+				var _oval = obj[_okey];
+
+				try { 
+					var _n = _val(_oval);
+					_val  = _n; }
+
+				catch (e) { 
+					_val = _oval; }
+			}
+
+			// Set the new value. try
+			if (_val != undefined || _val != null) obj[_okey] = _val;
+
+			// Remember how we will deal with the key outside these brackets?
+			// Well make sure that happens.
+			if (_key !== null || _key !== undefined) _nkey = _key;
 		}
+
+		// If we're mapping the key to a function, evaluate that function.
+		if (typeof _nkey === 'function') {
+			try { 
+				var _n = _nkey(_okey).toString();
+				_nkey  = _n; }
+
+			catch (e) { 
+				_nkey = _okey; }
+		}
+
+		// Don't override any pre-existing properties.
+		// No need to change if the new key is same as old.
+		if (obj.hasOwnProperty(_nkey) || 
+			_nkey === _okey) continue;
+
+		// Replace the old key with the new key.
+		obj[_nkey] = obj[_okey];
+		delete obj[_okey];
 	}
-};
+
+}; 
+
+
+
+
+// TODO: [String:], [Number:], [Boolean:] objects?
+
+// if (typeof map[_okey] === 'string' ||
+// 	typeof map[_okey] === 'number') {
+// 		_nkey  = map[_okey];
+// 	obj[_nkey] = obj[_okey];
+// 	delete obj[_okey];
+// 	continue;
+// 	
+// } else 
+
+// function complies (map, obj) {
+// 	for (var _key in map) {
+// 		var exp = new RegExp(_key);
+// 	}
+// }
+
+
+//
+// {'one' : 'two'}
+// 
+// 
+// 
+// 
+// 
 
 
 // module.exports.ob  = function (obj) {
 // 	function Obender (obj) {
 // 		return new Promise()
 // 	}
+// 	
+//	Obender.prototype.remap = function(map) {
+//		// body...
+//	};
 // 
 // 
 // 
--- test.js
+++ test.js
@@ -30,87 +30,212 @@ describe('Obender', function(){
 	});
 
 
-	describe('#remap()', function() {		
-		it('works for changing property names', function(){
-			remap(
-				{'Queue Name'		: 'queue_name',
-				 'Printer Name'		: 'printer_name',
-				 'Printer Model'	: 'printer_model',
-				 'Color'			: 'color',
-				 'DPI'				: 'dpi',
-				 'Duplex'			: 'duplex',
-				 '¢/Pg'			    : 'price_per_page'
-				}, object);
+	describe('#remap()', function() {	
 
-			var yofrankie = 
-				{ queue_name: 'wsh1',
-	        	  printer_name: 'WSH Browsing Library 1',
-	        	  printer_model: 'Xerox Phaser 4510DT',
-	        	  color: 'B/W',
-	        	  dpi: '600',
-	        	  duplex: 'Two-sided',
-	        	  price_per_page: '9'
-	        	}
+		it('doesn\'t crash on non-found or empty property names.', function(){
+				remap(
+					{'Queue Name'		: 'queue_name',
+					 'Printer Name'		: 'printer_name',
+					 'Printer Model'	: 'printer_model',
+					  4					: 'black_and_white', // Doesn't exist
+					  null				: 'dpi',
+					  undefined			: 'duplex',
+					 '¢/Pg'			    : 'price_per_page'
+					}, object);
 
-			expect(object).to.deep.equal(yofrankie);
+				var yofrankie = 
+					{ queue_name: 'wsh1',
+		        	  printer_name: 'WSH Browsing Library 1',
+		        	  printer_model: 'Xerox Phaser 4510DT',
+		        	  'Color': 'B/W',
+		        	  'DPI': '600',
+		        	  'Duplex': 'Two-sided',
+		        	  price_per_page: '9'
+		        	}
 
+				expect(object).to.deep.equal(yofrankie);
+		}); // ✓
+
+
+		describe('(on property names only)', function () {
+				
+			it('works for changing property names', function(){
+					remap(
+						{'Queue Name'		: 'queue_name',
+						 'Printer Name'		: 'printer_name',
+						 'Printer Model'	: 1,
+						 'Color'			: 2,
+						 'DPI'				: 'dpi',
+						 'Duplex'			: 3,
+						 '¢/Pg'			    : 'price_per_page'
+						}, object);
+
+					var yofrankie = 
+						{ queue_name: 'wsh1',
+			        	  printer_name: 'WSH Browsing Library 1',
+			        	  '1': 'Xerox Phaser 4510DT',
+			        	  '2': 'B/W',
+			        	  dpi: '600',
+			        	  '3': 'Two-sided',
+			        	  price_per_page: '9'
+			        	}
+
+					expect(object).to.deep.equal(yofrankie);
+			}); // ✓
+
+			it('can handle empty values', function(){
+					remap(
+						{'Queue Name'		: '',										// Will stay the same 
+						 'Printer Name'		: 'printer_name',							// First come first serve.
+						 'Printer Model'	: 'printer_name',							//   - will stay the same.
+						 'Color'			:  null,									// Will stay the same 
+						 'DPI'				:  undefined,								// Will stay the same 
+						 'Duplex lol'		:  null,									// Not found, stays the same
+						 '¢/Pg'			    : 'yo'
+						}, object);
+
+					var yofrankie = 
+						{ '': 'wsh1',
+			        	  printer_name: 'WSH Browsing Library 1',
+			        	  'Printer Model': 'Xerox Phaser 4510DT',
+			        	  'Color': 'B/W',
+			        	  'DPI': '600',
+			        	  'Duplex': 'Two-sided',
+			        	  yo: '9'
+			        	}
+
+					expect(object).to.deep.equal(yofrankie);
+			}); // ✓
+
+			it('can handle repetitions', function(){
+				var object = 
+					{'One'		: true,										
+					 'Two'		: true,										
+					 'Three'	: true,	
+					}
+
+					remap(
+						{'One'		: 'one',										
+						 'Two'		: 'two',							
+						 'Three'	: 'two',							
+						}, object);
+
+					var yofrankie = 
+						{'one'		: true,										
+						 'two'		: true,										
+						 'Three'	: true,	
+			        	}
+
+					expect(object).to.deep.equal(yofrankie);
+			}); // ✓
+
+			it('works for changing property names with function modifiers', function(){
+					remap(
+						{'Queue Name'		: function (value) { return 'dude'; },				
+						 'Printer Name'		: function (value) { return  5; },			// Empty. Will stay the same.		
+						 'Printer Model'	: function (value) { return  null; },		// Empty. Will stay the same.
+						 'Color'			: function (value) { return  undefined; },	// Empty. Will stay the same.
+						 'DPI'				: function (value) { throw   new Error('message'); },// Error. Will stay the same.
+						 'Duplex'			: function (value) { return ''; },			// Empty. Will stay the same.
+						 '¢/Pg'			    : function (value) { return 'dude'; }
+						}, object);
+
+					var yofrankie = 
+						{  dude: 'wsh1',
+			        	  '5': 'WSH Browsing Library 1',
+			        	  'Printer Model': 'Xerox Phaser 4510DT',
+			        	  'Color': 'B/W',
+			        	  'DPI': '600',
+			        	  '': 'Two-sided',
+			        	  '¢/Pg': '9'
+			        	}
+
+					expect(object).to.deep.equal(yofrankie);
+			}); // ✓
+
+			// TODO: it will use literals for arrays and empty objects?
 		});
 
-		it('doesn\'t crash on non-found properties', function(){
-			remap(
-				{'Queue Name'		: 'queue_name',
-				 'Printer Name'		: 'printer_name',
-				 'Printer Model'	: 'printer_model',
-				 'B/W'				: 'black_and_white', // Doesn't exist
-				 'DPI'				: 'dpi',
-				 'Duplex'			: 'duplex',
-				 '¢/Pg'			    : 'price_per_page'
-				}, object);
-
-			var yofrankie = 
-				{ queue_name: 'wsh1',
-	        	  printer_name: 'WSH Browsing Library 1',
-	        	  printer_model: 'Xerox Phaser 4510DT',
-	        	  'Color': 'B/W',
-	        	  dpi: '600',
-	        	  duplex: 'Two-sided',
-	        	  price_per_page: '9'
-	        	}
-
-			expect(object).to.deep.equal(yofrankie);
-		});
 
 
-		it('works for changing names and values', function(){
-			remap(
-				{'Queue Name'		:  'queue_name',
-				 'Printer Name'		:  'printer_name',
-				 'Printer Model'	:  'printer_model',
-				 'Color'			: {'color'			: function (value) { return value === 'Color'; } },
-				 'DPI'				:  'dpi',
-				 'Duplex'			: {'duplex' 		: function (value) { return value === "Two-sided"; } },
-				 '¢/Pg'			 	: {'price_per_page' : function (value) { return parseFloat(value) / 100; } }
-				}, object);	
+		describe('(on property names and object values)', function () {
+			it('works for changing names and values', function(){
+					remap(
+						{'Queue Name'		:  new Boolean(true),
+						 'Printer Name'		:  new Number(4),
+						 'Printer Model'	:  new String('printer_model'),
+						 'Color'			: {'color'			: function (value) { return value === 'Color'; } },
+						 'DPI'				:  'dpi',
+						 'Duplex'			: {'duplex' 		: function (value) { return value === 'Two-sided'; } },
+						 '¢/Pg'			 	: {'price_per_page' : function (value) { return parseFloat(value) / 100; } }
+						}, object);	
 
-			var expected = 
-				{ queue_name: 'wsh1',
-	        	  printer_name: 'WSH Browsing Library 1',
-	        	  printer_model: 'Xerox Phaser 4510DT',
-	        	  color: false,
-	        	  dpi: '600',
-	        	  duplex: true,
-	        	  price_per_page: 0.09
-	        	}
+					var expected = 
+						{ true: 'wsh1',
+			        	  '4': 'WSH Browsing Library 1',
+			        	  printer_model: 'Xerox Phaser 4510DT',
+			        	  color: false,
+			        	  dpi: '600',
+			        	  duplex: true,
+			        	  price_per_page: 0.09
+			        	}
 
-	        expect(object).to.deep.equal(expected);
-		});
+			        
 
-		it('doesn\'t crash from unproperly formatted mapping objects', function(){
-			assert(false);
-		});
+			        expect(object).to.deep.equal(expected);
+			});
 
-		it('Handles error on bending functions correctly', function(){
-			assert(false);
+			// it works for objects {[null, undefined]:
+			// 
+			// add mode {'key': func, 'val': func} ??
+
+			it('doesn\'t crash from unproperly formatted mapping objects', function(){
+					remap(
+						{'Queue Name'		:  'queue_name',
+						 'Printer Name'		:  'printer_name',
+						 'Printer Model'	:  'printer_model',
+						 'Color'			: {'color'			: 'lol?' },
+						 'DPI'				:  'dpi',
+						 'Duplex'			: {'duplex' 		: function (value) { return value === "Two-sided"; } },
+						 '¢/Pg'			 	:  100 
+						}, object);	
+
+					var expected = 
+						{ queue_name: 'wsh1',
+			        	  printer_name: 'WSH Browsing Library 1',
+			        	  printer_model: 'Xerox Phaser 4510DT',
+			        	  color: 'lol?',
+			        	  dpi: '600',
+			        	  duplex: true,
+			        	  '100': '9'
+			        	}
+
+			        expect(object).to.deep.equal(expected);
+			});
+
+			it('Handles error on bending functions correctly', function(){
+					remap(
+						{'Queue Name'		:  'queue_name',
+						 'Printer Name'		:  'printer_name',
+						 'Printer Model'	:  'printer_model',
+						 'Color'			: {'color'			: function (value) { return value === 'Color'; } },
+						 'DPI'				:  'dpi',
+						 'Duplex'			: {'duplex' 		: function () { var value = "error?"; } },
+						 '¢/Pg'			 	: {'price_per_page' : function (value) { throw new Error(); } }
+						}, object);	
+
+					var expected = 
+						{ queue_name: 'wsh1',
+			        	  printer_name: 'WSH Browsing Library 1',
+			        	  printer_model: 'Xerox Phaser 4510DT',
+			        	  color: false,
+			        	  dpi: '600',
+			        	  duplex: 'Two-sided',
+			        	  price_per_page: '9'
+			        	}
+
+			        expect(object).to.deep.equal(expected);
+			});
 		});
 	});
 });
